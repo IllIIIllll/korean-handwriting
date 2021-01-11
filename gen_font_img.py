468ab2b36af341616be4da8f41477dbc0f90f188
JISUNG PARK <llllllllll@kakao.com>
2021-01-11 22:08:28 +0900

    feat: Get font name
    폰트 이름 추출

--- gen_font_img.py
+++ gen_font_img.py
@@ -2,8 +2,13 @@
 # <llllllllll@kakao.com>
 # MIT License
 
+import os
+
 def dec_to_hex(n):
     return hex(n).replace('0x', '').upper()
 
 def hex_to_kr(h):
     return chr(int(h, 16))
+
+def get_font_name(font):
+    return os.path.basename(font).split('.')[0]
