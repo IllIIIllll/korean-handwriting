0bc20c50e2ef4285d780cf4ee7ee2c5abf0ca743
JISUNG PARK <llllllllll@kakao.com>
2021-01-12 02:17:03 +0900

    feat: Normalize data
    데이터 정규화
     - 0 -> -1
     - 255 -> 1

--- data/gen_font_img.py
+++ data/gen_font_img.py
@@ -21,12 +21,16 @@ def hex_to_kr(h):
 def get_font_name(font):
     return os.path.basename(font).split('.')[0]
 
+def normalization(data):
+    return (data - 127.5) / 127.5
+
 def font_to_img_array(uni, font):
     img = Image.new('L', (64, 64), color=255)
     draw = ImageDraw.Draw(img)
     kr = hex_to_kr(uni)
     draw.text((32, 32), kr, fill=0, anchor='mm', font=font)
-    return image.img_to_array(img, data_format='channels_last')
+    data = image.img_to_array(img, data_format='channels_last')
+    return normalization(data)
 
 def main():
     fonts = glob.glob('fonts/*.ttf')
