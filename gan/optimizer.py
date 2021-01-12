9839000b880b60652bfa2eadb9a1bca167a503f4
JISUNG PARK <llllllllll@kakao.com>
2021-01-13 02:30:02 +0900

    feat: Calculate Generator loss
    Generator loss 계산

--- gan/optimizer.py
+++ gan/optimizer.py
@@ -3,6 +3,12 @@
 # MIT License
 
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow import ones_like
 
 def generate_optimizer(learning_rate):
     return Adam(learning_rate), Adam(learning_rate)
+
+def generator_loss(fake):
+    cross_entropy = BinaryCrossentropy(from_logits=True)
+    return cross_entropy(ones_like(fake), fake)
