03e6e74077a07c6861352e171c3888925802aa81
JISUNG PARK <llllllllll@kakao.com>
2021-01-13 03:11:56 +0900

    feat: Calculate discriminator loss
    discriminator loss 계산

--- gan/optimizer.py
+++ gan/optimizer.py
@@ -4,7 +4,7 @@
 
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import BinaryCrossentropy
-from tensorflow import ones_like
+from tensorflow import ones_like, zeros_like
 
 def generate_optimizer(learning_rate):
     return Adam(learning_rate), Adam(learning_rate)
@@ -12,3 +12,9 @@ def generate_optimizer(learning_rate):
 def generator_loss(fake):
     cross_entropy = BinaryCrossentropy(from_logits=True)
     return cross_entropy(ones_like(fake), fake)
+
+def discriminator_loss(real, fake):
+    cross_entropy = BinaryCrossentropy(from_logits=True)
+    real_loss = cross_entropy(ones_like(real), real)
+    fake_loss = cross_entropy(zeros_like(fake), fake)
+    return real_loss + fake_loss
