b343d8f54c333c7b5059004f4f2459c5a5137e8f
JISUNG PARK <llllllllll@kakao.com>
2021-01-13 14:43:39 +0900

    feat: Add train function

--- train.py
+++ train.py
@@ -3,8 +3,12 @@
 # MIT License
 
 from gan.optimizer import *
+from data.generator import *
+from data.sampling import *
 
+from tqdm import tqdm
 from tensorflow import GradientTape
+from tensorflow import train as t
 
 def train_step(gen, dsc, gen_opt, disc_opt, font_images, noise):
     with GradientTape() as gen_tape, GradientTape() as dsc_tape:
@@ -21,3 +25,24 @@ def train_step(gen, dsc, gen_opt, disc_opt, font_images, noise):
 
     gen_opt.apply_gradients(zip(gen_gradient, gen.trainable_variables))
     disc_opt.apply_gradients(zip(disc_gradient, dsc.trainable_variables))
+
+def train(gen, dsc, samples, bs, epochs, noise):
+    gen_opt, disc_opt = generate_optimizer(1e-4)
+
+    checkpoint_dir = 'checkpoints/'
+    checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
+    checkpoint = t.Checkpoint(
+        generator_optimizer=gen_opt,
+        discriminator_optimizer=disc_opt,
+        generator=gen,
+        discriminator=dsc
+    )
+
+    for epoch in range(epochs):
+        dataset = DataGenerator(samples)
+        dataset = dataset.generate(bs)
+        for batch in tqdm(dataset, desc=f'{epoch+1} epochs'):
+            train_step(gen, dsc, gen_opt, disc_opt, batch, noise)
+
+        if not (epoch + 1) % 5:
+            checkpoint.save(file_prefix=checkpoint_prefix)
